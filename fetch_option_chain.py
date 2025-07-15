29c8d98b2475ab2507e298d7cbd4f119dd9b9421
Harishjangra09 <jangraharish9610@gmail.com>
2025-07-15 10:11:06 +0530

    Update fetch_option_chain.py

--- fetch_option_chain.py
+++ fetch_option_chain.py
@@ -8,7 +8,7 @@ load_dotenv()
 ACCESS_TOKEN = os.getenv("FYERS_ACCESS_TOKEN")
 CLIENT_ID = os.getenv("FYERS_CLIENT_ID")
 
-fyers = FyersModel(client_id=FYERS_CLIENT_ID, token=FYERS_ACCESS_TOKEN, log_path="")
+fyers = FyersModel(client_id=CLIENT_ID, token=ACCESS_TOKEN, log_path="")
 
 
 # === Generate NIFTY Option Symbols ===
